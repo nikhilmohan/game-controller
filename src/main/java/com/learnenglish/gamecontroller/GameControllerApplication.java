47baa3fcb30a391fa71ec1b385d824d67fb3e486
Nikhil Mohan <nikhil_mohan@infosys.com>
2020-05-02 16:35:21 +0530

    initial commit

---
+++ src/main/java/com/learnenglish/gamecontroller/GameControllerApplication.java
@@ -0,0 +1,13 @@
+package com.learnenglish.gamecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnenglish/gamecontroller/GameControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnenglish.gamecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
